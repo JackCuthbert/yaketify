343fdcc78bbe80b4e06f8d0de65bc2e34cf6365a
Jack Cuthbert <jckcthbrt@gmail.com>
2022-01-11 22:38:52 +1100

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import React from 'react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Yaketify</title>
+        <link rel="icon" type="image/png" href="/saxophone.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
