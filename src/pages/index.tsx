0991308a27a0c4a94e7426eab0785b8e27c296e6
Jack Cuthbert <jckcthbrt@gmail.com>
2022-01-11 22:33:32 +1100

    fix: clean up loading states, only load ffmpeg on the client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,78 +6,79 @@ import React, {
   ChangeEventHandler
 } from 'react'
 import type { NextPage } from 'next'
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+import { createFFmpeg, fetchFile, FFmpeg } from '@ffmpeg/ffmpeg'
 import { Spinner } from '../components/Spinner'
 
-const ffmpeg = createFFmpeg({
-  log: true,
-  corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js'
-})
-
 const YAKETY_FILE_URL = '/yakety.mp3'
+
+let ffmpeg: FFmpeg | null = null
+
+if (typeof window !== 'undefined') {
+  ffmpeg = createFFmpeg({
+    log: true,
+    corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js'
+  })
+}
 
 const Home: NextPage = () => {
   const [isTranscoding, setIsTranscoding] = useState(false)
   const fileElementRef = useRef<HTMLInputElement>(null)
   const videoElementRef = useRef<HTMLVideoElement>(null)
 
-  const [, /* transcodeMessage */ setTranscodeMessage] = useState<
-    string | undefined
-  >()
-  const [videoFile, setVideoFile] = useState<File | undefined>()
+  const [selectedFile, setSelectedFile] = useState<File | undefined>()
 
-  const [uploadedUrl, setUploadedUrl] = useState<string | undefined>()
-  const [transcodedUrl, setTranscodedUrl] = useState<string | undefined>()
+  const [sourceUrl, setSourceUrl] = useState<string | undefined>()
+  const [destinationUrl, setDestinationUrl] = useState<string | undefined>()
 
-  const handleVideoFile: ChangeEventHandler<HTMLInputElement> = event => {
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = event => {
     const file = event.currentTarget.files?.[0]
     if (file == null) return
 
-    setVideoFile(file)
+    setSelectedFile(file)
 
-    const url = URL.createObjectURL(file)
-
-    setUploadedUrl(url)
+    setSourceUrl(URL.createObjectURL(file))
+    setDestinationUrl(undefined)
   }
 
   useEffect(() => {
-    if (uploadedUrl == null) return
+    if (sourceUrl == null) return
 
     videoElementRef.current?.load()
-  }, [uploadedUrl])
+  }, [sourceUrl])
 
   const processVideo = useCallback(async () => {
-    if (uploadedUrl == null) return
+    if (ffmpeg == null) return
+
+    if (sourceUrl == null) return
     setIsTranscoding(true)
-    setTranscodeMessage('Loading ffmpeg-core.js')
     await ffmpeg.load()
 
-    setTranscodeMessage('Transcoding...')
-    ffmpeg.FS('writeFile', 'in.mp4', await fetchFile(uploadedUrl))
+    ffmpeg.FS('writeFile', 'in.mp4', await fetchFile(sourceUrl))
     ffmpeg.FS('writeFile', 'in.mp3', await fetchFile(YAKETY_FILE_URL))
     await ffmpeg.run('-i', 'in.mp4', '-i', 'in.mp3', '-shortest', 'out.mp4')
 
-    setTranscodeMessage('Done')
     const data = ffmpeg.FS('readFile', 'out.mp4')
 
-    setTranscodedUrl(
-      URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }))
-    )
+    const blob = new Blob([data.buffer], { type: 'video/mp4' })
+    setDestinationUrl(URL.createObjectURL(blob))
+
     setIsTranscoding(false)
     videoElementRef.current?.load()
-  }, [uploadedUrl])
+  }, [sourceUrl])
 
   const reset = (): void => {
-    setTranscodedUrl(undefined)
-    setVideoFile(undefined)
-    setUploadedUrl(undefined)
+    setSourceUrl(undefined)
+    setDestinationUrl(undefined)
+
+    setSelectedFile(undefined)
 
     if (fileElementRef?.current != null) {
       fileElementRef.current.value = ''
     }
   }
 
-  const isReady = !isTranscoding && transcodedUrl == null && uploadedUrl == null
+  const transcodeAvailable =
+    !isTranscoding && sourceUrl != null && destinationUrl == null
 
   return (
     <div className="min-h-screen bg-blue-50 pt-12 pb-12">
@@ -94,6 +95,7 @@ const Home: NextPage = () => {
           <div className="bg-white rounded-2xl shadow-xl shadow-blue-300/10 px-6 py-8 mb-12">
             <input
               ref={fileElementRef}
+              // TODO: Clean up this mess
               className="
                         block w-full text-xl text-gray-500
                           file:mr-4 file:py-2 file:px-4
@@ -104,7 +106,7 @@ const Home: NextPage = () => {
                         "
               type="file"
               accept="video/*"
-              onChange={handleVideoFile}
+              onChange={handleFileChange}
             />
           </div>
 
@@ -112,16 +114,16 @@ const Home: NextPage = () => {
             <button
               onClick={processVideo}
               className="inline-flex items-center space-x-4 bg-blue-500 shadow-lg shadow-blue-500/50 text-lg text-blue-50 px-6 py-3 rounded-lg disabled:opacity-80 disabled:cursor-not-allowed"
-              disabled={isReady || isTranscoding}
+              disabled={!transcodeAvailable}
             >
               <span>Yaketify!</span>
               {isTranscoding && <Spinner />}
             </button>
 
             <button
-              disabled={isReady}
+              disabled={sourceUrl == null || isTranscoding}
               onClick={reset}
-              className="text-gray-600 underline"
+              className="text-gray-600 underline disabled:cursor-not-allowed"
             >
               Reset
             </button>
@@ -131,14 +133,16 @@ const Home: NextPage = () => {
         <div className="text-center">
           <h2 className="text-2xl text-center">Preview</h2>
 
-          {transcodedUrl != null && videoFile != null && (
+          {destinationUrl != null && selectedFile != null && (
             <div className="mt-2">
               <p className="mb-4">
-                <code>{videoFile.name}</code> (
+                <code>{selectedFile.name}</code> (
                 <a
                   className="text-blue-700 underline"
-                  href={transcodedUrl}
-                  download={videoFile.name.split('.')[0] + '-yakety' + '.mp4'}
+                  href={destinationUrl}
+                  download={
+                    selectedFile.name.split('.')[0] + '-yakety' + '.mp4'
+                  }
                 >
                   download
                 </a>
@@ -146,13 +150,13 @@ const Home: NextPage = () => {
               </p>
               <div className="bg-white rounded-2xl shadow-xl shadow-gray-900/20 overflow-hidden">
                 <video className="w-full h-auto" ref={videoElementRef} controls>
-                  <source src={transcodedUrl} />
+                  <source src={destinationUrl} />
                 </video>
               </div>
             </div>
           )}
 
-          {transcodedUrl === undefined && (
+          {destinationUrl === undefined && (
             <p className="mt-6 text-gray-600">
               Select a video file and click the button!
             </p>
